8f5bf04caf4d792ae5e94708d92071b90394e4bb
Nguyễn Văn Tiến <tiennhot23@gmail.com>
2022-07-01 09:26:29 +0700

    base config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin',]
+  plugins: ['react-native-reanimated/plugin',
+  [
+    'module-resolver',
+    {
+      root: ['.'],
+      alias: {
+        '@src': './src',
+        '@common': './src/common',
+        '@constants': './src/constants',
+        '@styles': './src/styles',
+        '@components': './src/components',
+        '@animation': './src/animations',
+        '@containers': './src/containers',
+        '@menu': './src/menu',
+        '@naivgations': './src/naivgations',
+        '@reducers': './src/reducers',
+        '@screens': './src/screens',
+        '@translation': './src/translation'
+      }
+    }
+  ]]
 };
